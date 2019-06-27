47571006f8d574685033225df67adaed06a78301
Christophe Ducamp <christophe@ducamp.me>
2019-06-27 17:47:52 +0200

    Update date-filter.js
    localisation de la date en français

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -1,6 +1,6 @@
 // Stolen from https://stackoverflow.com/a/31615643
 const appendSuffix = n => {
-  var s = ['th', 'st', 'nd', 'rd'],
+  var s = [' ', 'er', ' ', ' '],
     v = n % 100;
   return n + (s[(v - 20) % 10] || s[v] || s[0]);
 };
@@ -8,7 +8,7 @@ const appendSuffix = n => {
 module.exports = function dateFilter(value) {
   const dateObject = new Date(value);
 
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
   const dayWithSuffix = appendSuffix(dateObject.getDate());
 
   return `${dayWithSuffix} ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
